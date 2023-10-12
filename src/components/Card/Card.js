eaac8adb65e8daa4b23dcb2f16de95c7db5aabdc
PrathameshT <prathmeshthombare20@gmail.com>
2023-10-12 16:08:35 +0530

    add changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./Card.css"
 
-function Card() {
+function Card({ title, description, img }) {
     return (
-      <div>
-        <h1>Card</h1>
+
+     <div className='card-container'>
+       <img className='image' src={img} />
+       <h2 className='title'>{title}</h2>
+       <p className='info'>{description}</p>
       </div>
+      
     )
   }
   
--- src/views/Blog/Blog.js
+++ src/views/Blog/Blog.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import{useState} from 'react'
+import Card from './../../Component/Card/Card'
 import './Blog.css';
 function Blog() {
+    const [blog ,setBlog]=useState( [
+        {
+            title: 'CheckEnv: Fast Detection of RPC Calls Between Environments Powered by Graphs',
+            description : 'Uber consists of a large number of loosely coupled microservices that interact with each other through remote procedure calls (RPCs). These RPC calls serve as the communication mechanism between services, allowing them to exchange data and invoke specific actions. The complex nature of the call chain, combined with the involvement of numerous services.',
+            img:'https://blog.uber-cdn.com/cdn-cgi/image/width=2048,quality=80,onerror=redirect,format=auto/wp-content/uploads/2023/09/Cover.jpg'
+        }]);
     return (
         <>
 
-            <div>
-                Blog Page
-            </div>
+       <div>
+         <div className='content'>
+         {
+            blog.map((blog,index)=>{
+                const {title,description,img}=blog;
+                
+                return (<Card 
+                        key={index} 
+                         img={img} 
+                         title={title} 
+                         description={description} />)
+                      })
+         }
+       </div>
+      </div>
+            
 
         </>
     )
