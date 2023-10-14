7c882b18a5a6179cea1de1a91627fb6dbc12015b
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2023-10-14 11:46:47 +0530

    store data in local storage from admin login page

--- src/views/Userlogin/Userlogin.js
+++ src/views/Userlogin/Userlogin.js
@@ -1,41 +1,71 @@
-import React , {useState} from "react";
+import React , {useEffect, useState} from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
-/*import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './Userlogin.css';
-function Userlogin() {
-    return (
-        <>
 
-            <div>
-                Userlogin Page
-            </div>
 
-        </>
-    )
-}*/
+      
 
-//import React , {useState} from "react";
-
-      <Navbar/>
-import "./Userlogin.css"
 
 const Userlogin = () => {
 
-    const[popupStyle, showPopup]= useState("hide")
+    const [datauser, setDatauser] = useState([]);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    const popup = () => {
-        showPopup ("login-popup")
-        setTimeout (() => showPopup ("hide"),3000)
+    console.log("username", username);
+    console.log("password", password);
+
+    const data = ()=>{
+        
+        const obj = {
+            username : username,
+            password : password,
+        }
+
+        const temparray = [...datauser,obj]
+        setDatauser(temparray)
+    //    const storedata =  localStorage.setItem(JSON.stringify(temparray));
+       localStorage.setItem('formData', JSON.stringify(temparray));
+       
     }
+
+    console.log("datauser" , datauser);
+    // const[popupStyle, showPopup]= useState("hide")
+
+    // const popup = () => {
+    //     showPopup ("login-popup")
+    //     setTimeout (() => showPopup ("hide"),3000)
+    // }
     return (
+        <>
+        <Navbar/>
         <div className="cover">
          <h1>Login</h1>
-         <input type="text" placeholder="username" />
-         <input type="password" placeholder="password" />
 
-        <div className="login-btn" onClick={popup}>Login</div>
+         <input type="text"
+          placeholder="username" 
+          value={username}
+          onChange={(e)=>{
+            setUsername(e.target.value)
+          }}
+          required
+          />
+
+         <input type="password"
+          placeholder="password" 
+          value={password}
+          onChange={(e)=>{
+            setPassword(e.target.value)
+          }}
+          required
+          />
+
+        <button className="login-btn"
+        onClick={()=>{
+            data()
+        }}
+        >Login</button>
         <p className="text">Or login using</p>
 
         <div className="alt-login">
@@ -43,12 +73,13 @@ const Userlogin = () => {
            <div className="google"></div>
         </div>
 
-        <div className={popupStyle}>
+        <div>
             <h3>Login Failed</h3>
             <p>Username or password incorrect</p>
         </div>
 
     </div>
+        </>
     )
 }
 
